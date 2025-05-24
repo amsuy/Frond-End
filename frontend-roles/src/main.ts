c7d64590e00abe39d79f290a967d9cfbf9ed33f1
Michael Elijah <maicolmir2024@gmail.com>
2025-05-23 23:12:21 -0600

    Subida completa del nuevo frontend desde la nueva ruta

---
+++ frontend-roles/src/main.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes'; // <- si defines las rutas aparte
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch(err => console.error(err));
